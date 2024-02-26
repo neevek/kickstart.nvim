755e13288db483817add8ea95e98a940aece4a69
neevek <i@neevek.net>
2024-02-26 23:07:17 +0800

    filter out C/C++ file for auto-formatting

--- lua/kickstart/plugins/autoformat.lua
+++ lua/kickstart/plugins/autoformat.lua
@@ -50,6 +50,11 @@ return {
           return
         end
 
+        -- Filter out C and C++ files
+        if vim.bo.filetype == 'c' or vim.bo.filetype == 'cpp' then
+          return
+        end
+
         -- Create an autocmd that will run *before* we save the buffer.
         --  Run the formatting command for the LSP that has just attached.
         vim.api.nvim_create_autocmd('BufWritePre', {
