548c52827ed69a8f2003fba80df33435c78cfb41
neevek <i@neevek.net>
2024-12-16 00:29:35 +0800

    update config

--- init.lua
+++ init.lua
@@ -288,6 +288,12 @@ require('lazy').setup({
             'nvim-treesitter/nvim-treesitter-textobjects',
         },
         build = ':TSUpdate',
+        config = function()
+            require("nvim-treesitter.configs").setup {
+                ensure_installed = { "c", "lua", "rust" },
+                highlight = { enable = true, }
+            }
+        end,
     },
 
     {
